fd2fa5c3bbe9181e0861de10a121d43b50983421
nguyenchung07 <151910751+nguyenchung07@users.noreply.github.com>
2023-11-24 15:25:40 +0700

    Add files via upload

--- bai1.cpp
+++ bai1.cpp
@@ -1,22 +1,17 @@
 #include<iostream>
 using namespace std;
 
-class Sum {
-    int a, b;
-public:
-    Sum(int a, int b) {
-        this->a = a;
-        this->b = b;
-    }
-    int TinhTong() {
-        return a + b;
-    }
+class tong{
+	public:
+		int songuyen(int a, int b){
+			return a+b;
+		}
 };
-
-int main() {
-    int A, B;
-    cin >> A>>B;
-    Sum s(A, B);
-    cout  << s.TinhTong() << endl;
-    return 0;
+int main(){
+	int A,B;
+	cin >>A>>B;
+	tong s;
+	int sum=s.songuyen(A,B);
+	cout << sum << endl;
+	return 0;
 }
